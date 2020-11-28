3892f605fdfabaecf5e4941d8ec9adaca02e5309
Gibran Ojeda <gibran.muppers@gmail.com>
2020-11-27 20:55:45 -0600

    Views

---
+++ muppers/resources/views/404.blade.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>404 | Muppers.com</title>
+    <meta property="og:title" content="Muppers">
+    <meta property="og:image" content="assets/img/muppers/muppers-logo.jpg">
+    <meta name="description" content="404 page not found">
+    <meta property="og:type" content="">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/css/pikaday.min.css">
+</head>
+
+<body><nav class="navbar navbar-dark navbar-expand-md fixed-top bg-dark navbar--apple">
+    <div class="container"><a class="navbar-brand nav-link" href="https://muppers.com"><img class="muppers-logo" alt="Muppers.com" src="assets/img/muppers/muppers-logo.jpg"> Muppers</a> <button data-toggle="collapse" data-target="#menu" class="navbar-toggler"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"><i class="la la-navicon"></i></span></button>
+        <div class="collapse navbar-collapse" id="menu">
+            <ul class="nav navbar-nav flex-grow-1 justify-content-between">
+                <li class="nav-item d-none d-xs-block d-md-block"></li>
+                <li class="nav-item"><a class="nav-link" href="https://muppers.com/contacto">Contáctanos</a></li>
+                <li class="nav-item"><a class="nav-link" href="https://muppers.com/servicios">Servicios</a></li>
+                <li class="nav-item"><a class="nav-link" href="https://muppers.com/portafolio">Proyectos</a></li>
+                <li class="nav-item"><a class="nav-link" href="https://muppers.com/preguntas-frecuentes">FAQ</a></li>
+            </ul>
+        </div>
+    </div>
+</nav><main class="page contact-page">
+    <section class="portfolio-block contact" style="background: #080808;color: rgb(255,255,255);">
+        <div class="container">
+            <!--
+VIEW IN FULL SCREEN MODE
+FULL SCREEN MODE: http://salehriaz.com/404Page/404.html
+
+DRIBBBLE: https://dribbble.com/shots/4330167-404-Page-Lost-In-Space
+-->
+
+<body class="bg-purple">
+
+  <div class="stars">
+    <div class="central-body">
+      <img class="image-404" src="assets/img/muppers/404.svg" width="300px">
+        <a href="https://muppers.com" class="btn-go-home" target="_blank">VOLVER AL INICIO</a>
+    </div>
+    <div class="objects">
+      <img class="object_rocket" src="assets/img/muppers/rocket.svg" width="40px">
+      <div class="earth-moon">
+        <img class="object_earth" src="assets/img/muppers/earth.svg" width="100px">
+        <img class="object_moon" src="assets/img/muppers/moon.svg" width="80px">
+      </div>
+      <div class="box_astronaut">
+        <img class="object_astronaut" src="assets/img/muppers/astronaut.svg" width="140px">
+      </div>
+    </div>
+    <div class="glowing_stars">
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+
+    </div>
+
+  </div>
+
+</body>
+        </div>
+    </section>
+</main><div class="footer-basic" style="  background: #080808;
+  color: rgb(255,255,255);
+">
+    <footer>
+        <div class="social" style="  color: rgb(211,152,152);
+"><a href="https://instagram.com/muppers_corp?igshid=1v53c7avxs70" target="_blank"><i class="fa fa-instagram" style="  color: #f2f5f8;
+"></i></a><a href="https://wa.me/525611566832/?text=Hola,%20me%20interesan%20sus%20servicios%2E" target="_blank"><i class="fab fa-whatsapp" style="color: #f2f5f8;
+"></i></a><a href="https://www.facebook.com/Muppers-104332594467041" target="_blank"><i class="fab fa-facebook-f" style="color: #f2f5f8;
+"></i></a></div>
+        <ul class="list-inline">
+            <li class="list-inline-item"><a href="https://muppers.com">Inicio</a></li>
+            <li class="list-inline-item"><a href="https://muppers.com/servicios">Servicios</a></li>
+            <li class="list-inline-item"><a href="https://muppers.com/sobre-nosotros">Sobre Nosotros</a></li>
+            <li class="list-inline-item"><a href="https://muppers.com/preguntas-frecuentes">FAQ</a></li>
+        </ul>
+        <p class="copyright">Muppers © 2020</p>
+    </footer>
+</div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/pikaday.min.js"></script>
+    <script src="assets/js/script.min.js"></script>
+</body>
+
+</html>
